004108b2c0edd4d2474bf7e7aa86ca2a987b654f
Sandeep Tyagi <sandeep@untrain.fit>
2025-06-08 20:19:20 +0530

    new look fdfdfd fdnewes df fdff sdfdf fff

--- src/pages/WorkoutsPage.jsx
+++ src/pages/WorkoutsPage.jsx
@@ -225,7 +225,7 @@ const Workouts = () => {
               return (
                 <div key={dateKey} className="timeline-date-wrapper">
                   <div
-                    className={`timeline-date-circle ${isActive ? 'active' : ''} ${(isFutureBeyondTomorrow) ? 'no-workout' : ''}`}
+                    className={`timeline-date-circle ${isActive ? 'active' : ''} ${(isFutureBeyondTomorrow) ? 'disabled' : ''}`}
                     onClick={() => handleDateSelect(dateKey)}
                     ref={(el) => (scrollRef.current[dateKey] = el)}
                   >
