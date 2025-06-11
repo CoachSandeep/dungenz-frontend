c50da17348ab8bb5b09aba8f97e6275a69c1e830
Sandeep Tyagi <sandeep@untrain.fit>
2025-06-11 17:15:21 +0530

    comment new new redirect dfdfd

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -2,6 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { isTokenValid } from './auth';
 
 const PrivateRoute = ({ children }) => {
+  console.log("🛡️ isTokenValid:", isTokenValid(token)); // Inside PrivateRoute
   const token = localStorage.getItem('token');
   return isTokenValid(token) ? children : <Navigate to="/login" />;
 };
