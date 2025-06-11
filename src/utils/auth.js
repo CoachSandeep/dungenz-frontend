488a82df9ef9fd66b6d85a51fbca5dd2407f6173
Sandeep Tyagi <sandeep@untrain.fit>
2025-06-11 17:08:21 +0530

    token news fdfdfd fdcode

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,13 +2,13 @@
 import { jwtDecode } from 'jwt-decode';
 
 export const isTokenValid = (token) => {
-  if (!token) return false;
-
-  try {
-    const { decoded } = jwtDecode(token);
-    const currentTime = Date.now() / 1000;
-    return decoded.exp > currentTime;
-  } catch (e) {
-    return false;
-  }
-};
+    if (!token) return false;
+  
+    try {
+      const decoded = jwtDecode(token); // ✅ FIXED
+      const currentTime = Date.now() / 1000;
+      return decoded.exp > currentTime;
+    } catch (e) {
+      return false;
+    }
+  };
