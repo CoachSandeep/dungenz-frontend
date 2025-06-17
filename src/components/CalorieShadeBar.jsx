52e4c71bae14085c16df68f6a5ea1d066ad44deb
Sandeep Tyagi <sandeep@untrain.fit>
2025-06-17 19:28:33 +0530

    calorie counter front news new toast dkfjfkfu dfffsd

--- src/components/CalorieShadeBar.jsx
+++ src/components/CalorieShadeBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './../styles/CalorieShadeBar.css';
 
 const CalorieShadeBar = ({ calorie }) => {
-  const minCal = 250;
+  const minCal = 200;
   const maxCal = 800;
   const percentage = Math.min(100, Math.max(0, ((calorie - minCal) / (maxCal - minCal)) * 100));
 
