12aa210b71dcbf1fa320d811145be9a16e55bbbd
Sandeep Tyagi <sandeep@untrain.fit>
2025-08-02 17:27:54 +0530

    push user checked ddf fdfd dfdf

--- src/components/admin/AdminCalendar.jsx
+++ src/components/admin/AdminCalendar.jsx
@@ -199,6 +199,19 @@ const AdminTimeline = () => {
   
     return filtered;
   };
+  const toggleWorkoutSelection = (workout, date, version) => {
+    const key = ${date}-${version};
+    const versionWorkouts = groupedWorkouts[date]?.versions[version] || [];
+    const allSelected = versionWorkouts.every(w => selectedWorkouts.includes(w._id));
+    const newSelection = [...selectedWorkouts];
+    versionWorkouts.forEach(w => {
+      const idx = newSelection.indexOf(w._id);
+      if (!allSelected && idx === -1) newSelection.push(w._id);
+      else if (allSelected && idx !== -1) newSelection.splice(idx, 1);
+    });
+    setSelectedWorkouts(newSelection);
+  };
+
   const filteredGrouped = getFilteredGrouped();
   const filteredDates = Object.keys(filteredGrouped).sort((a, b) => new Date(a) - new Date(b));
   
